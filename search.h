66395aede537b9b5b719291205a095a3d6f82a1a
sdsdsfsdfds <56422599+sdsdsfsdfds@users.noreply.github.com>
2020-07-05 10:11:16 +0800

    Add files via upload

---
+++ search.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+#include "seqlist.h"
+
+void DFS(AdjGraph *G,int v);		//递归深度优先遍历算法
+
+void DFS1(AdjGraph *G,int v);		//非递归深度优先遍历算法
+
+void BFS(AdjGraph *G,int v);		//广度优先遍历算法
+
+void Dijkstra(MatGraph g,int v);   //Dijkstra算法
