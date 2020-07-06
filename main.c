017e5cc5496c9a30142f890463c7fc9b60a41935
sdsdsfsdfds <3045170274@qq.com>
2020-07-06 23:35:00 +0800

    main.c

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
-int main()
-{
+#include <stdio.h>
+#include "stats.h"
+#include "search.h"
 
+int main(int argc, char *argv[])
+{
+    AdjGraph* g;
+    int u;
+    int v;
+    FILE *fp=fopen("dummy_test.txt", "r");
+    scanf("%d, %d", &u, &v);
+    while(/*flag == 0 */)
+    {
+
+        fprintf(fp, "%d", dummy_test_key.txt);
+    }
+
+    return 0;
 }
+
