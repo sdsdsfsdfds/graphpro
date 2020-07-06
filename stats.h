84ce9f5d4a9d538b76b6e2fb6d740e64b289fab3
sdsdsfsdfds <3045170274@qq.com>
2020-07-06 23:43:27 +0800

    stats.h

--- stats.h
+++ stats.h
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include "graph.h"
+
+int numberOfEdges(char name[]);     //返回图中边的数量
+int numberOfVertices(char name[]); //返回图中顶点的数量
+float freemanNetworkCentrality(char name[]);  //返回图中 Freeman's Network Centrality 值，此时默认图为无向图即可
+float closenessCentrality(char name[], int node); //返回图中 Closeness Centrality 值
+
