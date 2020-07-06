b906c3560787bae20ac71edab3c7ac857038ccaa
sdsdsfsdfds <3045170274@qq.com>
2020-07-06 23:41:02 +0800

    search.h

--- search.h
+++ search.h
@@ -10,3 +10,8 @@ void DFS1(AdjGraph *G,int v);		//非递归深度优先遍历算法
 void BFS(AdjGraph *G,int v);		//广度优先遍历算法
 
 void Dijkstra(MatGraph g,int v);   //Dijkstra算法
+
+char* shortestPath(int u, int v, char algorithm[], char name[])`
+{
+
+}
